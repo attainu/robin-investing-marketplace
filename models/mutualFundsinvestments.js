edf1563c3bd5488727904fc3ea1c4ba32c8e6e04
Shubham-Printe-au6 <shubhamprinte99@gmail.com>
2020-06-28 23:07:49 +0530

    added routes documentation

--- models/mutualFundsinvestments.js
+++ models/mutualFundsinvestments.js
@@ -7,37 +7,37 @@ const mutualFundsInvestmentSchema =  new Schema({
             ref: "users"//refering user database
         },
         Investments: [//making array of object for MF schemes
-            { 
-                MfId: {
-                    type: Schema.Types.ObjectId,
-                    ref: 'MutualFunds'
-                  },
-                SchemeId:{
-                type:String,
-                required: true,
-                trim: true
-            },
-            SchmeName: {
-                type: String,
-                required: true,
-                trim: true
-              },
+            {
+              MfId: {
+                  type: Schema.Types.ObjectId,
+                  ref: 'MutualFunds'
+                },
+              SchemeId:{
+              type:String,
+              required: true,
+              trim: true
+                },
+              SchmeName: {
+                  type: String,
+                  required: true,
+                  trim: true
+                },
               UnitsBought:{type:Number,
                 default: 0,
                 required: true,
                 trim: true},
-            Nav:{
-                type:Number,
-                default: 0,
-                required: true,
-                trim: true
-            },
-            AmountInvested: {
-                type: Number,
-                default: 0,
-                required: true,
-                trim: true
-              },
+              Nav:{
+                  type:Number,
+                  default: 0,
+                  required: true,
+                  trim: true
+                },
+              AmountInvested: {
+                  type: Number,
+                  default: 0,
+                  required: true,
+                  trim: true
+                },
             }
         ]
         // ,
@@ -49,11 +49,11 @@ const mutualFundsInvestmentSchema =  new Schema({
         // numberOfMutualFunds: {//total number of MF's
         //     type: Number,
         //     default: 0
-        // } 
+        // }
     })
-    
 
-  
+
+
 const MutualFundsInvestment = mongoose.model(
   "MutualFundsInvestment",
   mutualFundsInvestmentSchema,
